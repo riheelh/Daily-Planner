6c5caf956294a38b75596aa94e812a75dc924300
Riheel Hamoande <riheel83@gmail.com>
2021-03-06 17:02:18 -0600

    re-style table with bootstrap grid

--- app.js
+++ app.js
@@ -7,31 +7,33 @@ var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today)
 
 
-// create timeblocks 9 AM - 5 PM, bloacks are editiable
+// create timeblocks 9 AM - 5 PM blocks
+var wrapper = $('<container>').addClass('main-table');
+for(i=9; i<18; i++){
+    var row = $('<tr>').addClass('row');
+    wrapper.append(row);
+    row.append($('<td>').addClass('hour col-md-2 col-sm-1').text(i));
+    row.append($('<td>').addClass('present col-lg-8 col-md-4 col-sm-2'))
+    row.append($('<td>').addClass('saveBtn col-md-2 col-sm-1'))
+    
 
-var table = $('<table>').addClass('main-table');
-for(i=0; i<9; i++){
-    var row = $('<tr>').addClass('event-bar')
-    table.append(row);
-    var td =$('<td>').addClass('event-cell time-td').text('result'+ i);
-    row.append(td)
-    var td1 =$('<td>').addClass('event-cell1 text-td')
-    row.append(td1)
-    var td2 =$('<td>').addClass('event-cell1 save-td')
-    row.append(td2)
 }
-
-$('#table').append(table);
+$('.container').append(wrapper);
 
 // button to save the block data in local storage.
 // color code for blocks indicates past and present
 
+   
+// var wrapper = $('<container>').addClass('main-table');
+// for(i=9; i<18; i++){
+//     var row = $('<tr>').addClass('event-row')
+//     wrapper.append(row);
+//     var td =$('<td>').addClass('event-cell time-td').text(i);
+//     row.append(td)
+//     var td1 =$('<td>').addClass('event-cell1 text-td')
+//     row.append(td1)
+//     var td2 =$('<td>').addClass('event-cell1 saveBtn')
+//     row.append(td2)
+// }
 
-// 
-
-
-
-
-
-
-
+// $('.container').append(wrapper);
