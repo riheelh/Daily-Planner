398966c129c17598fecfb4302cd4a3e83c2b0dbe
Riheel Hamoande <riheel83@gmail.com>
2021-03-06 19:29:44 -0600

    add save icon

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 //global vars
-var eventTable = document.querySelector('table')
-
+var eventTable = document.querySelector('table');
 
 // display current day data on banner
 var today = moment().format("dddd, MMMM Do");
-$("#currentDay").text(today)
-
+$("#currentDay").text(today);
 
 // create timeblocks 9 AM - 5 PM blocks
 var wrapper = $('<container>').addClass('main-table');
+
 for(i=9; i<18; i++){
     var row = $('<tr>').addClass('row');
     wrapper.append(row);
-    row.append($('<td>').addClass('hour col-md-2 col-sm-1').text(i));
-    row.append($('<td>').addClass('present col-lg-8 col-md-4 col-sm-2'))
-    row.append($('<td>').addClass('saveBtn col-md-2 col-sm-1'))
-    
 
+    var time = $('<td>').addClass('hour col-md-2 col-sm-1').text(i);
+    var textArea = $('<td>').addClass('present col-lg-8 col-md-4 col-sm-2');
+    var save = $('<td>').addClass('saveBtn col-md-2 col-sm-1');
+    // var saveButton = document.createElement('Button')
+    var saveButton = $('<i>').addClass('fas fa-save')
+    
+    row.append(time, textArea, save);
+    save.append(saveButton)
+
+    
 }
 $('.container').append(wrapper);
 
