6f822c0ec4d6033dbc543d18d671b50ea56e836e
Riheel Hamoande <riheel83@gmail.com>
2021-03-08 17:25:22 -0600

    clean js file

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var apm;
 var wrapper = $('<container>').addClass('main-table');
 var now = moment().hour()
 
-// 
-
 //display current day data on banner
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
@@ -14,17 +12,12 @@ for(i=9; i<18; i++){
     var row = $('<tr>').addClass('row');
     wrapper.append(row);
 
-    // var apmConvert = moment(i, "hh").format('LT')
-
     var textArea = $('<td>').addClass('present col-lg-8 col-md-4 col-sm-2').append('<textarea id=text wrap="hard">');
-    // textArea.append('<textarea id=text wrap="hard">');
-    var someText = $('textarea').value
-    // console.log(someText)
     var save = $('<td>').addClass('saveBtn col-md-2 col-sm-1');
     var sbtn = $('<i>').addClass('subBtn fas fa-save fa-2x');
     // var saveButton = $('<i>').addClass('subBtn fas fa-save fa-2x');
-        var saveButton = $('<button>')
-        saveButton.append(sbtn)
+    var saveButton = $('<button>')
+    saveButton.append(sbtn)
         if(i < 12) {
             apm = 'AM'
         } else {
@@ -42,14 +35,11 @@ for(i=9; i<18; i++){
             textArea.addClass('future')
         }
   
-        $('.subBtn').click(function(e){
-            console.log(e);
+        $(saveButton).click(function(e){
+            console.log('clicked');
         });
 
    
-
-
-
     row.append(time, textArea, save);
     save.append(saveButton)
     
